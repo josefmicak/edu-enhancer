289a89f05b324991c6bd3a73290c0ce5efc7790e
josefmicak <71832264+josefmicak@users.noreply.github.com>
2022-07-05 12:17:39 +0200

    Vytvoření nového projektu, načítání šablon testů
    Vytvoření nového projektu, zprovozněno načítání šablon testů. U šablon testů je již využívána třída (TestTemplate).

---
+++ TaoEnhancer/Models/TestTemplateListModel.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel;
+
+namespace ViewLayer.Models
+{
+    public class TestTemplateListModel
+    {
+        private List<TestTemplate> testTemplates = new List<TestTemplate>();
+
+        public List<TestTemplate> TestTemplates { get { return testTemplates; } set { testTemplates = value; } }
+    }
+}
